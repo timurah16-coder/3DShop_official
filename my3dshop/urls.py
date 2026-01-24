b1a3ff6f8f29065d854cd6e294087614e0d6505e
timurah16-coder <timurah16@gmail.com>
2026-01-24 15:15:19 +0300

    Исправил импорт views и маршруты

--- my3dshop/urls.py
+++ my3dshop/urls.py
@@ -1,14 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('models3d.urls')),
+    path('', views.home, name='home'),
     path('order/<int:model_id>/', views.order, name='order'),
-    path('success/', views.success, name='success'),  # ← ЭТО ДОЛЖНО БЫТЬ!
+    path('success/', views.success, name='success'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
